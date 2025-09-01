27d965ddcabd6feadf207077e0db949d2c17b832
Ruslan Bihun <bigun.ruslan@gmail.com>
2025-09-01 10:43:27 +0200

    start work with views and urls

--- course_modelform/ex_1/views.py
+++ course_modelform/ex_1/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
+
+def index(request: HttpRequest):
+    if request.method == "GET":
+        context = {
+            
+        }
+        
+        return HttpResponse("Hello", context)
+        
