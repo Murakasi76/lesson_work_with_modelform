be5a9b036ffcad88fcbb31433027b6273339c46d
Ruslan Bihun <bigun.ruslan@gmail.com>
2025-09-02 11:20:06 +0200

    add Book in forms and add class FormBook

--- course_modelform/ex_1/forms.py
+++ course_modelform/ex_1/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from ex_1.models import Author
+from ex_1.models import Author, Book
 
 
 class FormAuthor(forms.ModelForm):
@@ -7,3 +7,14 @@ class FormAuthor(forms.ModelForm):
     class Meta:
         model = Author
         fields = ["first_name", "last_name"]
+
+
+
+class FormBook(forms.ModelForm):
+    
+    class Meta:
+        model = Book
+        fields = ["title", "page", 'author']
+        
+        
+        
